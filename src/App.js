e4b68d3a885438c17445510801f6010ad09cdc6d
Toby J. Henderson <bigtob@gmail.com>
2019-04-04 02:01:41 -0500

    Updated Front End Styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank.js';
 import './App.css';
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 120,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       },
+//       color: {
+//         value: "#ffffff" 
+//         }
+//       }
+//     }
+//   }
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-	<Navigation />
+      <Particles 
+              params={{particlesOptions}} 
+     />
+  <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm />
+    {/*<FaceRecognition /> */}
+
+   
 	       
       </div>
     );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Rank = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'Toby your current rank is...'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
