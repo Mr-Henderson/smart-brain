e419091660362c4b4b85f9d3b8c11a57ee87e1c9
Toby J. Henderson <bigtob@gmail.com>
2019-04-05 03:44:40 -0500

    updated image form and button scripts

--- src/App.js
+++ src/App.js
@@ -19,35 +19,33 @@ const particlesOptions = {
   }
 }
 
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 120,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       },
-//       color: {
-//         value: "#ffffff" 
-//         }
-//       }
-//     }
-//   }
-
-
-
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
-      <Particles 
-              params={{particlesOptions}} 
-     />
+      <Particles className='particles'
+          params={particlesOptions}
+        />
   <Navigation />
     <Logo />
     <Rank />
-    <ImageLinkForm />
+    <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit} />
     {/*<FaceRecognition /> */}
 
    
