05698a9c1654f29a40fb04cc3740677b90ca8b12
Toby J. Henderson <bigtob@gmail.com>
2019-04-10 03:34:57 -0500

    bugfix front end and npm audit fix

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-
-const Rank = ({ onInputChange, onButtonSubmit }) => {
+const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className='white f3'>
-        {'Toby your current rank is...'}
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
       </div>
     </div>
   );
